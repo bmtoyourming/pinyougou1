1043ae3eaa4c0e83c32a60ba6a9829c10424317e
lwq <dasdas d@das.com>
2019-03-28 16:01:20 +0800

    添加了web的几个配置文件

---
+++ pinyougou1-web/pinyougou1-manager-web/src/main/java/com/pinyougou1/manager/controller/BrandController.java
@@ -0,0 +1,12 @@
+package com.pinyougou1.manager.controller;
+
+import com.pinyougou1.service.BrandService;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BrandController {
+
+    private BrandService brandService;
+}
