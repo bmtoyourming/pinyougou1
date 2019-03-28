04e553a08147b972d5f075ed4b1032296b5c057e
lwq <dasdas d@das.com>
2019-03-28 18:25:36 +0800

    在service整合了junit,添加了spring-test依赖

--- pinyougou1-pojo/src/main/java/com/pinyougou1/pojo/Brand.java
+++ pinyougou1-pojo/src/main/java/com/pinyougou1/pojo/Brand.java
@@ -1,48 +1,46 @@
 package com.pinyougou1.pojo;
 
-import javax.persistence.*;
 import java.io.Serializable;
-@Table(name="tb_brand")
-public class Brand implements Serializable {
-    /**
-     * 品牌实体类
-     *
-     * @author lee.siu.wah
-     * @version 1.0
-     * <p>File Created at 2019-03-26<p>
-     */  // 品牌id
-    @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
-    @Column(name="id")
-    private Long id;
-    // 品牌名称
-    @Column(name="name")
-    private String name;
-    // 品牌首字母
-    @Column(name="first_char")
-    private String firstChar;
 
+import javax.persistence.*;
+
+/**
+ * 品牌实体类
+ * @author LEE.SIU.WAH
+ * @email lixiaohua7@163.com
+ * @date 2017年12月1日 下午4:59:52
+ * @version 1.0
+ */
+@Table(name="tb_brand")
+public class Brand implements Serializable{
+	private static final long serialVersionUID = -8315881228799842049L;
+	/** 主键ID */
+	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private Long id;
+	/** 品牌名称 */
+	@Column(name="name")
+    private String name;
+    /** 品牌首字母 */
+	@Column(name="first_char")
+    private String firstChar;
+    /** setter and getter method */
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
-        this.name = name;
+        this.name = name == null ? null : name.trim();
     }
-
     public String getFirstChar() {
         return firstChar;
     }
-
     public void setFirstChar(String firstChar) {
-        this.firstChar = firstChar;
+        this.firstChar = firstChar == null ? null : firstChar.trim();
     }
 }
