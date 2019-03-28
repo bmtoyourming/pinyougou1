54ed95f6146fb6763d0b2fb838afd1c3e260aa0c
lwq <dasdas d@das.com>
2019-03-28 16:12:04 +0800

    编写了品牌类的控制器

--- pinyougou1-web/pinyougou1-manager-web/src/main/java/com/pinyougou1/manager/controller/BrandController.java
+++ pinyougou1-web/pinyougou1-manager-web/src/main/java/com/pinyougou1/manager/controller/BrandController.java
@@ -1,12 +1,24 @@
 package com.pinyougou1.manager.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.pinyougou1.pojo.Brand;
 import com.pinyougou1.service.BrandService;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class BrandController {
+import java.util.List;
 
+@RestController
+@RequestMapping("/brand")
+public class BrandController {
+    @Reference(timeout = 10000)
     private BrandService brandService;
+    @GetMapping("/findAll")
+    public List<Brand> findAll(){
+        System.out.println(brandService);
+        return brandService.findAll();
+    }
 }
